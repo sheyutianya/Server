802324d7b6d84610133a78f93369b10e23807dcb
sheyutianya <tianyasheren@Gmail.com>
2015-05-28 17:53:56 +0800

    add Http Server

---
+++ httpServer/HttpServer/HttpServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpServer httpServer = new MyHttpServer(8080);
+            Thread thread = new Thread(new ThreadStart(httpServer.listen));
+            thread.Start();
+        }
+    }
+}
